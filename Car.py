fa8b1e4a36409df143b1b90762471f0910fbeb18
caBrer24 <69688063+caBrer24@users.noreply.github.com>
2020-10-19 16:50:18 -0500

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,24 @@
+started = False
+while True:
+    comma = input('> ').lower()
+    if comma == "start":
+        if started:
+            print('Already started you foo')
+        else:
+            started = True #Is the car started? yes or Simon ya arranco
+            print('Car started... Ready to go!')
+    elif comma == "help":
+        print('start - to start the car')
+        print('stop - to stop the car')
+        print('quit - to exit the game')
+
+    elif comma == "stop":
+        if not started:
+            print('Cannot stop twice!')
+        else:
+            started = False
+            print('Car has stopped')
+    elif comma == "quit":
+        break
+    else:
+        print('I do not get that')
