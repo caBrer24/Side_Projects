d559b9027e15e8817f48ae5f5c7a9ed62cf31c98
caBrer24 <69688063+caBrer24@users.noreply.github.com>
2020-10-19 17:42:20 -0500

    Update Textual_Driving.py

--- Textual_Driving.py
+++ Textual_Driving.py
@@ -1,24 +1,26 @@
 started = False
 while True:
-    comma = input('> ').lower()
-    if comma == "start":
+    command = input('> ').lower()
+    if command == "start":
         if started:
-            print('Already started you foo')
+            print('Already started!')
         else:
-            started = True #Is the car started? yes or Simon ya arranco
+            started = True #Is the car started?
             print('Car started... Ready to go!')
-    elif comma == "help":
+    
+    elif command == "help": #Display helps command
         print('start - to start the car')
         print('stop - to stop the car')
         print('quit - to exit the game')
 
-    elif comma == "stop":
+    elif command == "stop": #Stops car
         if not started:
             print('Cannot stop twice!')
         else:
             started = False
             print('Car has stopped')
-    elif comma == "quit":
+   
+    elif command == "quit": #Exits game
         break
     else:
         print('I do not get that')
